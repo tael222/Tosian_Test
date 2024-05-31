5988f7f599c238dc4e444f65b0d92f5718817ebe
Tael222 <138008560+tael222@users.noreply.github.com>
2024-05-31 16:04:45 +0900

    BasicScene 에서 디버그 로그 출력
    BasicScene 에서 디버그 로그를 출력합니다.
    메시지 : Hello Unity! By PSK

---
+++ Assets/Scripts/PSK_Test_Script.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PSK_Test_Script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+         Debug.Log("Hello Unity! By PSK");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
